537a70f614d6724730fd85d4a991a38cd1f1209d
Konstantin Bezgatsev <gelandewagen1@gmail.com>
2023-03-22 18:49:43 +0100

    Company

--- src/main/java/company/ComparatorRating.java
+++ src/main/java/company/ComparatorRating.java
@@ -2,5 +2,12 @@ package company;
 
 //Сделать отдельный компаратор который сортирует по рейтингу
 
-public class ComparatorRating {
+import java.util.Comparator;
+
+public class ComparatorRating implements Comparator<Employee> {
+
+    @Override
+    public int compare(Employee e1, Employee e2) {
+         return e1.getRating().compareTo(e2.getRating());
+    }
 }
--- src/main/java/company/Main.java
+++ src/main/java/company/Main.java
@@ -2,11 +2,17 @@ package company;
 
 //В классе Мэйн надо прописать только один метод с параметром количеством сотрудников который с генерирует весь этот процесс и выведет на экран Сотрудника с зп ДО и После
 
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Employee> employeeList = Generator.generateEmployee(5);
+        List<Employee> employeeList = Generator.generateEmployee(10);
+        for (Employee employee : employeeList) {
+            System.out.println(employee);
+        }
+        System.out.println("////////////////////////////////////////////////////////");
+        employeeList.sort(new ComparatorRating());
         for (Employee employee : employeeList) {
             System.out.println(employee);
         }
